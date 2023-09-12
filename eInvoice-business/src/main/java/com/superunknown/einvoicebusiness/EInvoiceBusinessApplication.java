ec58ab6655a71d7e3e68dd60a8e367036fee1981
zgalusic <zgalusic@tis.hr>
2023-09-12 12:03:16 +0200

    create business module with basic setup

---
+++ eInvoice-business/src/main/java/com/superunknown/einvoicebusiness/EInvoiceBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.superunknown.einvoicebusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EInvoiceBusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EInvoiceBusinessApplication.class, args);
+    }
+
+}
---
+++ eInvoice-business/src/test/java/com/superunknown/einvoicebusiness/EInvoiceBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superunknown.einvoicebusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EInvoiceBusinessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
