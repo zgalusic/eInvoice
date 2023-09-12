1305258324db42a06077bf45694e1ab60ccbba48
zgalusic <zgalusic@tis.hr>
2023-09-12 13:11:43 +0200

    remove unnecessary imports

--- eInvoice-business/src/main/java/com/superunknown/einvoicebusiness/api/rest/client/crm/CrmRestClientServiceImpl.java
+++ eInvoice-business/src/main/java/com/superunknown/einvoicebusiness/api/rest/client/crm/CrmRestClientServiceImpl.java
@@ -2,13 +2,10 @@ package com.superunknown.einvoicebusiness.api.rest.client.crm;
 
 import com.superunknown.einvoicebusiness.api.rest.client.RestClient;
 import com.superunknown.einvoicebusiness.api.rest.client.crm.model.CrmResponse;
-import com.superunknown.einvoicebusiness.api.rest.client.crm.model.Customer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
