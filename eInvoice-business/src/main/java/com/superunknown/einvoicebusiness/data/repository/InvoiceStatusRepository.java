1456e96c7e574604228e3786c5c060da6fff4fd7
zgalusic <zgalusic@tis.hr>
2023-09-17 18:01:09 +0200

    create repository for invoice status

--- eInvoice-business/src/main/java/com/superunknown/einvoicebusiness/data/repository/InvoiceStatusRepository.java
+++ eInvoice-business/src/main/java/com/superunknown/einvoicebusiness/data/repository/InvoiceStatusRepository.java
@@ -1,9 +1,9 @@
 package com.superunknown.einvoicebusiness.data.repository;
 
 import com.superunknown.einvoicebusiness.data.entity.InvoiceStatus;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InvoiceStatusRepository extends CrudRepository<InvoiceStatus, Long> {
+public interface InvoiceStatusRepository extends ListCrudRepository<InvoiceStatus, Long> {
 }
